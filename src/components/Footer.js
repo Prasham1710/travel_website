0b56ad8036ac7382fe019ceb6c89093085d91dd4
Prasham1710 <jainprasham17@gmail.com>
2023-01-08 05:03:20 +0530

    FOOT

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
     <div className='w-full bg-gray-100 py-16'>
       <div className='max-w-[1240px] mx-auto flex flex-col px-4'>
         <div className='sm:flex text-center justify-between items-center'>
-          <h1>BEACHES.</h1>
+          <h1>BAYWATCH</h1>
           <div className='flex justify-between w-full sm:max-w-[280px] my-4'>
             <a href='https://twitter.com/makemytrip?lang=en'>< FaTwitter className='icon' /></a>
             <a href='https://www.facebook.com/makemytrip/'><FaFacebook className='icon' /></a>

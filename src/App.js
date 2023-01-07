2907db2d78915c5a1ac8b36c5f39d8cc536bf30e
Prasham1710 <jainprasham17@gmail.com>
2023-01-07 14:52:56 +0530

    n

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>happyu<div>
-    </div>nie</div>
-=======
     <div>happy<div>
     </div>nie
     <p>hii</p></div>
     
->>>>>>> 15f7d7e37ba7af9a74a52841d7dd775ce4dc8325
+
   )
 }
 

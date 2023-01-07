f45528bcf30cabce9d1458fed28672f0f2f3f0d9
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-07 21:50:01 +0530

    image

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -11,7 +11,7 @@ const Destinations = () => {
        <h1>All-Inclusive Resorts</h1>
        <p>On the Caribean's Beat Beaches</p>
        <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4'>
-           <img className='w-full h-full object-cover' src={b2} alt='/' />
+           <img className='w-full h-full object-cover col-span-2 md:col-span-3 row-span-2' src={b2} alt='/' />
            <img className='w-full h-full object-cover' src={b4} alt='/'/>
            <img className='w-full h-full object-cover' src={b3} alt='/' />
            <img className='w-full h-full object-cover' src={b1} alt='/' />

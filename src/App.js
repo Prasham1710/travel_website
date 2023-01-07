9ab5dcf30322e89e5ded5c3d09aa6065662f5e48
Prasham1710 <jainprasham17@gmail.com>
2023-01-08 03:15:49 +0530

    button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import Destinations from './components/Destinations'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
 
-const App = () => {
+import React from 'react';
+import Carousel from './components/Carousel';
+import Destinations from './components/Destinations';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Search from './components/Search';
+import Selects from './components/selects';
+
+function App() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Destinations />
+      <Search />
+      <Selects />
+      <Carousel />
+      <Footer />
     </div>
-    
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
        <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
        <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
            <h1>First Class Travel</h1>
-           <h2 className='py-4'>Top 1% locations Worldwide</h2>
+           <h2 className='py-4'>Top 1.1% locations Worldwide</h2>
            <form className='flex justify-between items-center max-w-[700px] mx-auto w-full border p-1 rounded-xl text-black bg-gray-100/90'>
                <div>
                <input className='bg-transparent w-[300px] sm:w-[400px] font-[poppins] focus:outline-none' type='text' placeholder='Search Destination' />

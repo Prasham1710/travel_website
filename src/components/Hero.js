b689ea7eecde1ffe42f1c5639ee96dc51c9ba63d
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-07 19:55:19 +0530

    background

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,11 @@ const Hero = () => {
   return (
     <div className='w-full h-screen relative'>
         <video className='w-full h-full object-cover' src={beachvideo} autoPlay loop muted />
-
+       <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
+       <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
+           <h1>First Class Travel</h1>
+           <h2>Top 1% locations Worldwide</h2>
+       </div>
     </div>
   )
 }

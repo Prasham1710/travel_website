185fca2ef4b21c8b77a14e9d4d4b5f46c6dabbc5
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2023-01-07 18:19:22 +0530

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,30 +38,31 @@ const Navbar = () => {
            
           </div>
 
-          {/*Mobile menu drop down*/}
-          <div onClick={handleNav} className={nav?'absolute left-0 top-0 w-full bg-gray-200 px-5 py-10 flex flex-col' : 'absolute left-[-100%]'}>
-            <ul>
-            <h1>BEACHESS.</h1>
-            <button><li className='border-b'>Home</li></button>
-            <button><li className='border-b'>Destination</li></button>
-            <button><li className='border-b'>Travel</li></button>
-            <button><li className='border-b'>View</li></button>
-            <button><li className='border-b'>Book</li></button>
-            <div className='flex flex-col'>
-              <button className='my-6'>Search</button>
-              <button>Account</button>
-            </div>
-            <div className='flex justify-between my-6'>
-              <FaFacebook className='icon'/>
-              <FaTwitter className='icon'/>
-              <FaInstagram className='icon'/>
-              <FaYoutube className='icon'/>
-              <FaPinterest className='icon'/>
-            </div>
-            </ul>
+      {/* Mobile menu dropdown */}
+      <div onClick={handleNav} className={nav ? 'absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+        <ul>
+          <h1>BEACHES.</h1>
+          <li className='border-b'><button>Home</button></li>
+         <li className='border-b'><button>Destination</button></li>
+          <li className='border-b'><button>Travel</button></li>
+          <li className='border-b'><button>View</button></li>
+          <li className='border-b'><button>Book</button></li>
+          <div className='flex flex-col'>
+            <button className='my-6 p-3 border bg-gradient-to-r from-blue-900 to-sky-500 text-white rounded-2xl'>Search</button>
+            <button className='p-3 border bg-gradient-to-r from-blue-900 to-sky-500 text-white rounded-2xl '>Account</button>
           </div>
+          <div className='flex justify-between my-6'>
+            <FaFacebook className='icon' />
+            <FaTwitter className='icon' />
+            <FaYoutube className='icon' />
+            <FaPinterest className='icon' />
+            <FaInstagram className='icon' />
+          </div>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default Navbar

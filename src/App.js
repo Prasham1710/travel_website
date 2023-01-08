56ad17803f7dd9203433be0772ce81210987b6f0
Prasham1710 <jainprasham17@gmail.com>
2023-01-08 15:20:02 +0530

    hero

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import Destinations from './components/Destinations'
-import Hero from './components/Hero'
-import Navbar from './components/Navbar'
-import Search from './components/Search'
-import Select from './components/Select'
-import Footer from './components/Footer'; 
-import Carousel from './components/Carousel'
-import { BrowserRouter, Routes,Route } from "react-router-dom";
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <BrowserRouter>
-      <Routes>
+  import React from 'react'
+  import Destinations from './components/Destinations'
+  import Hero from './components/Hero'
+  import Navbar from './components/Navbar'
+  import Search from './components/Search'
+  import Select from './components/Select'
+  import Footer from './components/Footer'; 
+  import Carousel from './components/Carousel'
+  import { BrowserRouter, Routes,Route } from "react-router-dom";
+  function App() {
+    return (
+      <div>
+        <BrowserRouter>
+        <Navbar />
+        <Routes>
+        <Route path='/' element={<Hero/>}/>
         <Route path='/destination' element= {<Destinations/>}/>
-        </Routes></BrowserRouter>
-      <Search />
-      <Select />
-      <Carousel />
-      <Footer />
+        </Routes>
+        <Search />
+        <Select />
+        <Carousel />
+        <Footer />
+        </BrowserRouter>
+      </div>
+    );
+  }
 
-    </div>
-  );
-}
-
-export default App;
+  export default App;
